79397bf3c582d1c25045288be429d9d32c67559c
Mivenya <amanda.d.dasilva@gmail.com>
2024-06-15 13:10:33 -0700

    Update to begin building data visualization class and member

--- capstone_project/Analyzer.py
+++ capstone_project/Analyzer.py
@@ -4,6 +4,8 @@ import sys
 import pandas as pd
 import numpy as np
 from random import sample
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder, StandardScaler
 
@@ -21,7 +23,7 @@ def describe(dataset: pd.DataFrame):
     """
     print(dataset.describe()) 
     
-# Class 1 = Data Manipulation any below are members
+# Class 1 = Data Manipulation class, any below are members
 
 class DataManipulation:
     def __init__(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -85,7 +87,7 @@ class DataManipulation:
 
         Returns: pd.DataFrame: The DataFrame with standardization.
         """
-        scaler = StandardScaler()
+        scaler = StandardScaler().set_output(transform="pandas")
         self.df = scaler.fit_transform(self.df)
         return self.df
 
@@ -121,6 +123,55 @@ class DataManipulation:
         """
         return self.df
     
+# Class 2 = Data Visualization class, any below are members
+
+class DataVisualization:
+    def __init__(self, df: pd.DataFrame) -> pd.DataFrame:
+        self.df = df
+
+    def plot_correlationMatrix(self, df: pd.DataFrame) -> plt.show(): 
+        """Using a heatmap show correlation matrix of features of dataset
+
+        Args:
+            df (pd.Dataframe): The name of the dataframe to use for visualization
+
+        Returns: plt.show(): The heatmap visualization of the features
+        """
+        plt.figure(figsize=(12,12))
+        correlation = sns.heatmap(df.corr(), annot=True).set_title('Correlated Heatmap of diamond features with price')
+        return plt.show()
+    
+    def plot_histograms_categorical(self, df: pd.DataFrame) -> plt.show():
+        """Using a histogram to visualilze the dataset
+
+        Args:
+            df (pd.Dataframe): The name of the dataframe to use for visualization
+
+        Returns: plt.show(): The histogram visualization of the features
+        """
+        dataset_d = df.groupby(['color_D'])['price'].mean()
+        dataset_e = df.groupby(['color_E'])['price'].mean()
+        dataset_f = df.groupby(['color_F'])['price'].mean()
+        dataset_g = df.groupby(['color_G'])['price'].mean()
+        dataset_h = df.groupby(['color_H'])['price'].mean()
+        dataset_i = df.groupby(['color_I'])['price'].mean()
+        dataset_j = df.groupby(['color_J'])['price'].mean()
+
+        fig, axs = plt.subplots(2, 4, figsize=(12,12))
+        axs[0, 0].bar(dataset_d.index, dataset_d.values, width=0.5, color='green')
+        axs[0, 0].set_title('Pric')
+        axs[0, 1].bar(dataset_sex.index, dataset_sex.values, color='purple')
+        axs[0, 1].set_title('Charges by Sex')
+        axs[1, 0].bar(dataset_region.index, dataset_region.values, color='cyan')
+        axs[1, 0].set_title('Charges by Region')
+        axs[1, 1].bar(dataset_children.index, dataset_children.values, color='orange')
+        axs[1, 1].set_title('Charges based on Children')
+
+        for ax in axs.flat:
+            ax.set(xlabel='Feature', ylabel='Charges')
+
+
+
 if __name__== "__main__":
 
     #testing read dataset function
@@ -128,52 +179,50 @@ if __name__== "__main__":
     df = read_dataset(dataset_path=absolute_path)
 
     #testing describe function
-    describe(dataset=df)
+    #describe(dataset=df)
     
+   
     #testing DataManipulation class
     my_data_manipulation = DataManipulation(df=df)
 
     #testing drop na
     data_df = my_data_manipulation.drop_missing_data(df)
-    print(data_df)
+    #print(data_df)
 
     #testing drop column
     cleaned_df = my_data_manipulation.drop_column(column_names="Unnamed: 0")
-    print(cleaned_df)
+    #print(cleaned_df)
 
     #testing feature one hot encoder
     encoded_df = my_data_manipulation.encode_features(column_names="color")
-    print(encoded_df)
+    #print(encoded_df)
 
     #testing label encoder
     lblencoded_df = my_data_manipulation.encode_label(column_names="cut")
     lblencoded_df = my_data_manipulation.encode_label(column_names="clarity")
-    print(lblencoded_df)
+    #print(lblencoded_df)
 
     #testing label encoder
     scaled_df = my_data_manipulation.standardize(df=df)
-    print(scaled_df)
+    #print(scaled_df)
     
     #testing dataset shuffle
-    #shuffled_df = my_data_manipulation.shuffle(df=df)
+    shuffled_df = my_data_manipulation.shuffle(df=df)
     #print(shuffled_df)
 
     #testing dataset 50% sample
-   # sampled_df = my_data_manipulation.sample(df=df)
+    sampled_df = my_data_manipulation.sample(df=df)
     #print(sampled_df)
 
     #testing retrieve data
-   # retrieved_df = my_data_manipulation.retrieve_data(df=df)
-    #print(retrieved_df)
-
-#Data Manipulation
-#Data Visualization
-# member output is dataframe as attribute
+    retrieved_df = my_data_manipulation.retrieve_data(df=df)
+    print(retrieved_df)
 
 
+    #testing DataVisualization class
+    my_data_visualization = DataVisualization(df=df)
 
-
-
-
-# Class 2 = Plotting and any below plots are members
+    #testing plotting of correlation matrix
+    corr_matrix = my_data_visualization.plot_correlationMatrix(df=retrieved_df)
+    print(corr_matrix)
 
